41a23311143b3ffe09aad96ad6fcf50b86f9cd33
ShadowyBandit <73307315+ShadowyBandit@users.noreply.github.com>
2021-03-08 12:12:46 -0600

    Variable names

--- io_mesh_geometry/import_geometry.py
+++ io_mesh_geometry/import_geometry.py
@@ -9,17 +9,17 @@ import xml.etree.ElementTree as ET
 # Loader
 
 class ModelLoader:
-    def load_geometry(self, model_filepath, debug_mode, displacement, rotation, scale):
-        model_dir = os.path.dirname(model_filepath) #Directory of the .geometry file
-        model_filename = os.path.basename(model_filepath) #Name of the .geometry file
-
-        visual_filename = '%s.visual' % os.path.splitext(model_filename)[0] #Name of the visual file
-        primitives_filename = '%s.geometry' % os.path.splitext(model_filename)[0] #Name of the geometry file
+    def load_geometry(self, file_path, debug_mode, displacement, rotation, scale):
+        file_dir = os.path.dirname(file_path) #Directory of the .geometry and.visual file pair
         
-        visual_filepath = os.path.join(model_dir, visual_filename) #Full path of the visual file
-        primitives_filepath = os.path.join(model_dir, primitives_filename) #Full path of the primitives file
-
-        if os.path.exists(visual_filepath) and os.path.exists(primitives_filepath): #If visual exists
+        base_filename = os.path.basename(file_path) #Base name of the selected file
+        visual_filename = '%s.visual' % os.path.splitext(base_filename)[0] #Extended name of the .visual file
+        geometry_filename = '%s.geometry' % os.path.splitext(base_filename)[0] #Extended name of the .geometry file
+        
+        visual_path = os.path.join(file_dir, visual_filename) #Full path of the .visual file
+        geometry_path = os.path.join(file_dir, geometry_filename) #Full path of the .geometry file
+        
+        if os.path.exists(visual_path) and os.path.exists(geometry_path): #If both files exist
             
         else: #If visual doesn't exist
             print('[Import Error] %s does not exist. Check the directory.' %visual_filename)
