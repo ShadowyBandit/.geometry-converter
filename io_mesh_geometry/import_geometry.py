79ba40ed5d167957d5a16f2c006d6300c0c5c545
ShadowyBandit <73307315+ShadowyBandit@users.noreply.github.com>
2021-03-23 21:19:20 -0600

    PROGRESS

--- io_mesh_geometry/import_geometry.py
+++ io_mesh_geometry/import_geometry.py
@@ -10,10 +10,15 @@ from struct import unpack
 # Loader
 
 class ModelLoader:
-    _counts = []
-    
     _visual_file = None
     _geometry_file = None
+    
+    _counts = [] #Number of vertex types, index types, vertex blocs, index blocs, collision blocs, and armor blocs
+    _table_positions = [] #Locations of the info tables for vertices and indices
+    _section_positions = [] #Locations of each section
+
+    _vertex_info = []
+    _index_info = []
     
     def load_geometry(self, file_path, debug_mode, displacement, rotation, scale):
         file_dir = os.path.dirname(file_path) #Directory of the selected file
@@ -33,5 +38,26 @@ class ModelLoader:
             self._visual_file = open(visual_path, 'rb')
             self._geometry_file = open(geometry_path, 'rb')
 
-            for i in range(6):
+            for i in range(6): #Read number of vertex types, index types, vertex blocs, index blocs, collision blocs, and armor blocs
                 self._counts.append(unpack('<i', self._geometry_file.read(4))[0])
+
+            for i in range(2): #Read info table locations
+                self._table_positions.append(unpack('<i', self._geometry_file.read(4))[0])
+                self._geometry_file.seek(4, 1)
+
+            for i in range(4): #Read section locations
+                self._section_positions.append(unpack('<i', self._geometry_file.read(4))[0])
+                self._geometry_file.seek(4, 1)
+
+            for i in range(self._counts[2]): #Read vertex info
+                self._vertex_info.append({
+                    'name'  : self._geometry_file.read(4).hex(),
+                    'type_location' : unpack('<i', self._geometry_file.read(4))[0],
+                    'position' : unpack('<i', self._geometry_file.read(4))[0],
+                    'vertices_count'   : unpack('<i', self._geometry_file.read(4))[0]
+                })
+
+        print(self._counts)
+        print(self._table_positions)
+        print(self._section_positions)
+        print(self._vertex_info)
